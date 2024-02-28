67007fbd7fa6e8ccc610a613e15052554f14dd46
Siva004-123 <sivakumar.22ad@kct.ac.in>
2024-02-28 22:04:04 +0530

    Siva's commit

--- interface_app.py
+++ interface_app.py
@@ -5,23 +5,30 @@ import os
 if "no_of_csv" not in st.session_state:
     st.session_state.no_of_csv = 1
 
-# Identify target variable types have chaai
+# Identification feature type
 def class_identification(column_name):
         if column_name in df.columns:
             unique_values_count = df[column_name].nunique()
-            data_type = df[column_name].dtype
+            flag=0
+            try :
+                str(df.iloc[0:1,'column_name'])
+                flag=1
+            except ValueError:
+                flag=0
 
-            if unique_values_count < len(df):
+            if unique_values_count < len(df)/2:
                 #  suggesting classification
-                target_type = "Classification"
-            elif data_type == "object":
-                # suggesting classification
-                target_type = "Classification"
+                target_type = "Categorical"
+            elif flag==1:
+                target_type='Categorical'
+            
             else:
                 #  assume regression
-                target_type = "Regression"
+                target_type = "Numerical"
             
             return target_type
+        
+#Navigation/Side bar
 
 with st.sidebar:
     st.title("Automated machine learning")
@@ -42,14 +49,23 @@ if choice == "Upload":
         st.session_state.no_of_csv += 1
 
         target_variable = st.selectbox("Enter the target column",df.columns)
-        idependent_df=df.drop(target_variable, axis=1)
+        
         #current dataset name
+        idependent_df=df.drop(target_variable, axis=1)
+        
+        class_of_target=class_identification(target_variable)
+        if class_of_target=='Categorical':
+            st.write("A Classification Problem")
+        else:
+            st.write('A Regression Problem')
         idependent_columns=st.multiselect("enter the columns",idependent_df.columns)
         
-       
-        
-        
-
+        class_type_of_coloumn=dict()
+        for names in idependent_columns:
+            #contains the type(Catogorical/Regression) of independent features
+            class_type_of_coloumn[names]=class_identification(names)
+            
+        print(class_type_of_coloumn)
         
 if choice=='Preprocessing':
     st.dataframe(df)
