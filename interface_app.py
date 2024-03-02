98396de63b8878ea98066c346ca0f6f57cfc433e
Siva004-123 <sivakumar.22ad@kct.ac.in>
2024-03-02 16:17:05 +0530

    Imputed the null values

--- interface_app.py
+++ interface_app.py
@@ -2,6 +2,38 @@ import streamlit as st
 import pandas as pd
 import os
 
+
+
+def is_empty_file_size(filename):
+
+  return os.path.getsize(filename) < 3
+
+
+
+if os.path.exists('independent_data.csv'):
+    if not is_empty_file_size('independent_data.csv'):
+        df_2=pd.read_csv('independent_data.csv')
+    else:
+        df_2=pd.Dataframe()
+        
+if os.path.exists('regression_data.csv'):
+    if not is_empty_file_size('regression_data.csv'):
+        regression_df=pd.read_csv('regression_data.csv')
+    else:
+        regression_df=pd.DataFrame()
+        
+if os.path.exists('encoding_needed_data.csv'):
+    if not is_empty_file_size('encoding_needed_data.csv'):
+        encoding_needed_df=pd.read_csv('encoding_needed_data.csv')
+    else:
+        encoding_needed_df=pd.DataFrame()
+        
+if os.path.exists('encoding_not_needed_data.csv'):
+    if not is_empty_file_size('encoding_not_needed_data.csv'):
+        encoding_not_needed_df=pd.read_csv('encoding_not_needed_data.csv')
+    else:
+        encoding_not_needed_df=pd.DataFrame()
+
 if "no_of_csv" not in st.session_state:
     st.session_state.no_of_csv = 1
 # Initialize dictionaries to store information
@@ -76,11 +108,13 @@ if choice == "Upload":
             for i in df.columns:
                 if i not in selected_columns_info:
                     to_be_removed_columns.append(i)
-            
-            df_2=df.drop(to_be_removed_columns,axis=1)
-            
+            df_2=pd.DataFrame()
             regression_df=pd.DataFrame()
             classification_df=pd.DataFrame()
+            encoding_needed_df=pd.DataFrame()
+            encoding_not_needed_df=pd.DataFrame()
+            
+            df_2=df.drop(to_be_removed_columns,axis=1)
             
             for i in df_2.columns:
                 if independent_info[i]=='Regression':
@@ -88,26 +122,41 @@ if choice == "Upload":
                 else:
                     classification_df[i]=df_2[i]
              
-            encoding_needed_df=pd.DataFrame()
-            encoding_not_needed_df=pd.DataFrame()
-            
             for i in classification_df.columns:
                 if independent_info[i+'_encoding']=='Needed':
                     encoding_needed_df[i]=classification_df[i]
                 else:
                     encoding_not_needed_df[i]=classification_df[i]
+    
+            df_2.to_csv("independent_data.csv",index=None)
+            regression_df.to_csv("regression_data.csv",index=None)
+            encoding_needed_df.to_csv("encoding_needed_data.csv",index=None)
+            encoding_not_needed_df.to_csv("encoding_not_needed_data.csv",index=None)
 
 if choice == 'Preprocessing':
-    # Store DataFrame
-    preprocessing_df = df
+    
+    #removal of null values in categorical datasets
+    if not encoding_needed_df.empty:
+        for i in encoding_needed_df.columns:
+            mode_value = encoding_needed_df[i].mode()[0]
+
+    # Fill the missing values with mode
+            encoding_needed_df[i].fillna(mode_value, inplace=True)
+            df_2[i]=encoding_needed_df[i]
+            
+    if not encoding_not_needed_df.empty:
+        for i in encoding_not_needed_df.columns:
+            mode_value = encoding_not_needed_df[i].mode()[0]
+
+    # Fill the missing values with mode
+            encoding_not_needed_df[i].fillna(mode_value, inplace=True)
+            df_2[i]=encoding_not_needed_df[i]
+        st.dataframe(encoding_not_needed_df)
+        
+    
 
 if choice == 'Model Fitting':
     pass
 
 if choice == 'Download':
     pass
-
-# Print stored information
-print("Target variable info:", target_info)
-print("Independent variable info:", independent_info)
-print("Selected columns info:", selected_columns_info)
