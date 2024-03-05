506d5bec3dca2001ac7332ebff4dd86526d6c4e5
Siva004-123 <sivakumar.22ad@kct.ac.in>
2024-03-05 18:45:47 +0530

    model down

--- machine_learning.py
+++ machine_learning.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import pickle
+from interface_app import *
 
 def identify_classification_regression(target_df):
     unique_values_count = target_df.nunique()[0]
@@ -7,10 +9,12 @@ def identify_classification_regression(target_df):
         return "Classification"
     else:
         return "Regression"
-def machine_learning(df_2,target_df):
-    df=pd.concat([df_2,target_df],axis=1)
-    target_type=identify_classification_regression(target_df)
-    if target_type=='Classification':
+
+def machine_learning(df_2, target_df):
+    df = pd.concat([df_2, target_df], axis=1)
+    target_type = identify_classification_regression(target_df)
+    
+    if target_type == 'Classification':
         if st.button("Run Modelling"): 
             from pycaret.classification import setup, compare_models, pull, save_model
             setup(df, target=target_df.columns[0])
@@ -18,7 +22,7 @@ def machine_learning(df_2,target_df):
             st.dataframe(setup_df)
             best_model = compare_models()
             compare_df = pull()
-            save_model(best_model, 'best_model')
+            save_model(best_model, 'best_model.pkl')  # Saving model to a file
             st.dataframe(compare_df)
     else:
         from pycaret.regression import setup, compare_models, pull, save_model
@@ -28,5 +32,20 @@ def machine_learning(df_2,target_df):
             st.dataframe(setup_df)
             best_model = compare_models()
             compare_df = pull()
-            save_model(best_model, 'best_model')
+            save_model(best_model, 'best_model.pkl')  # Saving model to a file
             st.dataframe(compare_df)
+
+    # Add a download button for the best model
+    if st.button("Download Best Model"):
+        model_filename = 'best_model.pkl'
+        with open(model_filename, 'rb') as file:
+            model = file.read()
+        st.download_button(
+            label="Download Model",
+            data=model,
+            file_name=model_filename,
+            mime="application/octet-stream"
+        )
+
+
+
